918cd89440f1fdebe5864bb08afd865ec3e26865
Gaeon <gaeon0413@gmail.com>
2025-05-29 14:27:04 +0900

    feat : project repository 추가

--- src/main/java/com/skala/decase/domain/project/repository/ProjectRepository.java
+++ src/main/java/com/skala/decase/domain/project/repository/ProjectRepository.java
@@ -1,4 +1,8 @@
 package com.skala.decase.domain.project.repository;
 
-public class ProjectRepository {
+import com.skala.decase.domain.project.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+
 }
